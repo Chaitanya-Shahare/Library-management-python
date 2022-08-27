e3d1d24ea059a2788a2740c4afdc859b8d58be91
CHAITANYA SHAHARE <chaitanyashahare@CHAITANYAs-MacBook-Air.local>
2022-08-27 12:10:32 +0530

    added returnbook method to library.py and made a premitive logging system *barebones*

--- library.py
+++ library.py
@@ -1,22 +1,60 @@
+# Instructions for continueing this project
+# test all the functions there seems to be a problem in returnbook method
+# also see how to different logs line up 
 class Library:
     def __init__(self, listofbooks = [], library_name= 'Default library'):
         self.library_name = library_name
         self.listofbooks = listofbooks
-        self.log = {}
+        self.lendlog = {}
+        self.returnlog = {}
 
     def displaybooks(self):
         return self.listofbooks
 
+    def addbook(self, book_name = ''):
+        self.listofbooks.append(book_name)
+
     def lendbook(self, book_name = '' , borrower = ''):
-        self.log[book_name] = borrower
+        for n in self.listofbooks:
+            if n == book_name:
+                flag = 1
+                break
+            else:
+                flag = 0
+                continue
+        if flag == 1:
+            self.lendlog[book_name] = borrower
+            self.listofbooks.remove(book_name)
+        else:
+            print("Error! Book not found.")
+
+    def returnbook(self, book_name = '', borrower = ''):
+        for k,v in self.lendlog:
+            if k == book_name:
+                flag = 1
+                break
+            else:
+                flag = 0
+                continue
+        if flag == 1:
+            self.returnlog[book_name] = borrower
+            self.listofbooks.append(book_name)
+        else: 
+            print("Error! This book was not lent.")
+
+
+
 
 chaitanya = Library(['a','b','c','d'], 'chaitanya')
 shahare = Library(['x','y','z'], 'shahare')
+
 print(chaitanya.listofbooks)
-print(chaitanya.library_name)
-print(chaitanya.displaybooks())
-chaitanya.lendbook('a', 'abrakadabra')
-print(chaitanya.log)
-chaitanya.lendbook('c', 'lsadjfasf')
-print(chaitanya.log)
+chaitanya.addbook('xxx')
+print(chaitanya.listofbooks)
+chaitanya.lendbook('xxx', 'abrakadabra')
+print(chaitanya.lendlog, chaitanya.listofbooks)
+chaitanya.lendbook('sdkjfha', 'lksadfj')
+
+chaitanya.returnbook('xxx', 'abrakadabra')  #***
+print(chaitanya.returnlog, chaitanya.lendlog, chaitanya.listofbooks)
 
